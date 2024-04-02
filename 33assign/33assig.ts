48a4b16c3efb28602a3df59101a4a1a8cbca7714
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:14:41 +0500

    Add files via upload

---
+++ 33assign/33assig.ts
@@ -0,0 +1,62 @@
+
+/*
+
+Ordinal Numbers: Ordinal numbers indicate their position in a array, 
+such as 1st or 2nd. 
+Most ordinal numbers end in th, except 1, 2, and 3.
+• Store the numbers 1 through 9 in a array.
+
+• Loop through the array.
+
+• Use an if-else chain inside the loop to print the proper ordinal ending for each number. 
+Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", 
+and each result should be on a separate line.
+
+
+     */ 
+
+let data3 : number[] = [1,2,3,4,5,6,7,8,9,10];
+let i : number ;
+for(i=0;data3[i]<10;i++){
+    if (data3[i]==1){
+      // data2.push(i); 
+     console.log("1st") ;
+    }
+    else   if (data3[i]==2){
+     console.log("2nd") ;
+    }
+    else   if (data3[i]==3){
+     console.log("3rd") ;
+    }
+    else
+    {
+    console.log(`${data3[i]}th `);  
+    }
+
+ 
+}
+
+/*
+/* other try
+let data2 : number[] = [];
+let i : number ;
+for(i=0;i<10;i++){
+    if (i==1){
+      // data2.push(i); 
+     console.log("1st") ;
+    }
+    else if (i==2){
+     console.log("2nd") ;
+    }
+    else if (i==3){
+     console.log("3nd") ;
+    }
+    else
+    {
+    console.log(`${i}th `);  
+    }
+
+ data2.push(i); 
+}
+console.log(data2);  
+ok2 */
