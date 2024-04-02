38bfdcb826ebc6045e57f5a074fcea4e766e84f2
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 11:52:02 +0500

    Add files via upload

---
+++ 14guestarray/14augarray.js
@@ -0,0 +1,4 @@
+var Guest = ['Kareem', 'Raheem'];
+for (var i = 0; i < Guest.length; i++) {
+    console.log('I welcome you Sir!' + Guest[i]);
+}
---
+++ 14guestarray/14augarray.ts
@@ -0,0 +1,4 @@
+let Guest :string[] = ['Kareem','Raheem'];
+for(let i=0; i<Guest.length;i++){
+    console.log('I welcome you Sir!' + Guest[i]);
+}
