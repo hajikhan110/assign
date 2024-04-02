9bf51a41c10c93cd401a2913c0c603073e5413a8
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:29:17 +0500

    Add files via upload

---
+++ 9favnumber/9fav.js
@@ -0,0 +1,4 @@
+var fav_number = 9;
+var msg = 'here is your fav number ' + fav_number;
+//console.log(fav_number);
+console.log(msg);
---
+++ 9favnumber/9fav.ts
@@ -0,0 +1,4 @@
+const fav_number :number = 9;
+const msg :string = 'here is your fav number ' +  fav_number;
+//console.log(fav_number);
+console.log(msg);
