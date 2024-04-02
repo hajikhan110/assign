13a0491269396e7ddeb940dc0ed737f7c63b6a8e
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:27:19 +0500

    Add files via upload

---
+++ 6assig/6assig.ts
@@ -0,0 +1,18 @@
+// // Stripping Names: Store a person’s name, and 
+// include some whitespace characters at the beginning 
+// and end of the name. Make sure you use each character 
+// combination, "\t" and "\n", at least once.
+//  Print the name once, so the whitespace around the
+//   name is displayed. Then print the name after
+//    striping the white spaces.
+// Windows: \r\n carriage return followed by a newline character.
+// Linux: \n just a newline character.
+// Older Macs: \r just a carriage return character.
+//where g and m are for global and multiline flags.
+///[\r\n]+/gm
+let person: string='\thaji khan\n sahito'
+console.log(`${person}`);
+person = person.replace(/(\r\n|\n|\r|\t)/gm, "");
+//person = person.replace(/(\r\n)+/gm, "");
+console.log(`After`);
+console.log(`${person}`);
