81b61cb3f5d67b6b847b0b861d90d4c3e3048f2d
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:12:51 +0500

    Add files via upload

---
+++ 31assign/31assign.ts
@@ -0,0 +1,18 @@
+
+
+
+// No Users: Add an if test to 
+// Exercise 28 to make sure the list of 
+// users is not empty.
+
+let user : string[]= ['kareem','raheem'];
+
+if(user.length === 0){
+    console.log('we need to find some users');
+}else
+{
+    user =[];
+  // If the list is empty, print the message We need to find some users!  
+console.log(' the list is empty, We need to find some users!  ');
+
+}
