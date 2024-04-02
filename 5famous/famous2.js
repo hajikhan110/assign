1fecedcd4d3328e017163dab6eb87cff202e6dad
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:26:45 +0500

    Add files via upload

---
+++ 5famous/famous2.js
@@ -0,0 +1,6 @@
+var famous_person = 'William Shakespeare';
+var message = 'A rose by any other name would smell as sweet. ';
+//console.log('${famous}');
+console.log("".concat(message));
+//console.log(``);
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
---
+++ 5famous/famous2.ts
@@ -0,0 +1,7 @@
+let famous_person :string = 'William Shakespeare';
+
+let message :string ='A rose by any other name would smell as sweet. ';
+//console.log('${famous}');
+console.log(`${message}`);
+//console.log(``);
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
