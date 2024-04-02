8ed793b84f4ec18628e4cf6c78a7eb24c76b7170
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:01:26 +0500

    Add files via upload

---
+++ 20array/20array.js
@@ -0,0 +1,6 @@
+var data = ['saudi_Arabia', 'k2', 'Manghopeer'];
+var data2 = [];
+console.log(data);
+data2.push('seaview');
+data2.push('hyderabad');
+console.log(data2);
---
+++ 20array/20array.ts
@@ -0,0 +1,6 @@
+let data : string[] =['saudi_Arabia','k2','Manghopeer'];
+let data2 : string[] = [];
+console.log(data);
+data2.push('seaview');
+data2.push('hyderabad');
+console.log(data2);
