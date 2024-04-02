19b29eabb708d53b374ffd7587cf5f0f0e724e7d
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:03:08 +0500

    Add files via upload

---
+++ 22errorarray/22error.js
@@ -0,0 +1,5 @@
+var countries = ["Pakistan", 'uae', 'usa', 'canada'];
+//delebertly error no item at index 18
+console.log(countries[18]);
+//see array items
+console.log(countries);
---
+++ 22errorarray/22error.ts
@@ -0,0 +1,7 @@
+let countries : string[]=["Pakistan",'uae','usa','canada'];
+//delebertly error no item at index 18
+console.log(countries[18]);
+//see array items
+console.log(countries);
+
+
