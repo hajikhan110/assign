3f8135c71d0aecdd31c97f16d3b34a574a4649ed
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 11:50:05 +0500

    Add files via upload

---
+++ 12arraygreetins/12arraymsg.js
@@ -0,0 +1,5 @@
+var nms = ['Karee', 'Raheem'];
+var msg = 'Hey welcome there Mr.';
+for (var i = 0; i < nms.length; i++) {
+    console.log(msg + nms[i]);
+}
---
+++ 12arraygreetins/12arraymsg.ts
@@ -0,0 +1,6 @@
+let nms :string[]=['Karee','Raheem'];
+const msg:string='Hey welcome there Mr.';
+for(let i=0; i<nms.length; i++){
+
+    console.log(msg + nms[i]);
+}
