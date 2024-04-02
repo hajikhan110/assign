77ae7749f1258514e279a10b7b2e8b77c0bb570c
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:04:12 +0500

    Add files via upload

---
+++ 23iftests/23iftests.js
@@ -0,0 +1,14 @@
+var car = 'subaru';
+//if tests with data
+console.log("Is car == 'subaru' ? I predict true.");
+console.log(car == 'subaru');
+console.log(car === 'subaru');
+console.log(car !== 'subaru1');
+console.log(car !== 'subaru3');
+console.log(car !== 'subaru4');
+console.log("Is car == 'subaru' ? I predict false.");
+console.log(car == 'mehran');
+console.log(car === 'subaru0');
+console.log(car == 'subaru1');
+console.log(car == 'subaru3');
+console.log(car == 'subaru4');
---
+++ 23iftests/23iftests.ts
@@ -0,0 +1,17 @@
+let car : string = 'subaru';
+//if tests with data
+
+console.log("Is car == 'subaru' ? I predict true.")
+
+console.log(car == 'subaru');
+console.log(car === 'subaru');
+console.log(car !== 'subaru1');
+console.log(car !== 'subaru3');
+console.log(car !== 'subaru4');
+console.log("Is car == 'subaru' ? I predict false.")
+
+console.log(car == 'mehran');
+console.log(car === 'subaru0');
+console.log(car == 'subaru1');
+console.log(car == 'subaru3');
+console.log(car == 'subaru4');
