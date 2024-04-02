048fd0f3ef22e7db21ac69aece359c2bd66540e5
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 11:51:04 +0500

    Add files via upload

---
+++ 13array2/13array2.js
@@ -0,0 +1,4 @@
+var trans = ['Motorcycle', 'Car', 'Train', 'Cycle'];
+for (var i = 0; i < trans.length; i++) {
+    console.log('I like transport like' + trans[i]);
+}
---
+++ 13array2/13array2.ts
@@ -0,0 +1,4 @@
+let trans :string[] = ['Motorcycle','Car','Train','Cycle'];
+for(let i=0; i<trans.length;i++){
+    console.log('I perfer transport like' + trans[i]);
+}
