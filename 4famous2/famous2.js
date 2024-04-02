675bc8c2d8c88795983eb2f4ec5ad74159217963
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:26:09 +0500

    Add files via upload

---
+++ 4famous2/famous2.js
@@ -0,0 +1,6 @@
+var author = 'William Shakespeare';
+var fmessage = 'A rose by any other name would smell as sweet. ';
+//console.log('${famous}');
+console.log("".concat(author, " , once said \" ").concat(fmessage, "\""));
+//console.log(``);
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
---
+++ 4famous2/famous2.ts
@@ -0,0 +1,7 @@
+let author :string = 'William Shakespeare';
+
+let fmessage :string ='A rose by any other name would smell as sweet. ';
+//console.log('${famous}');
+console.log(`${author} , once said " ${fmessage}"`);
+//console.log(``);
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
