ac91baf75bd37d80b26a47414b1017feb1cff402
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:05:43 +0500

    Add files via upload

---
+++ 25color/25color.js
@@ -0,0 +1,11 @@
+//Alien Colors #1: Imagine an alien was just shot down
+// in a game. Create a variable called alien_color and 
+//assign it a value of 'green', 'yellow', or 'red'.
+var acolor = 'green';
+if (acolor === 'green') {
+    console.log("You earned score 5 !");
+}
+acolor = 'red';
+if (acolor === 'green') {
+    console.log("You earned score 5!");
+}
