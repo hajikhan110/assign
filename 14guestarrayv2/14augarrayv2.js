fee79ab96df08c1568fe36eaece25419ad19f712
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 11:52:46 +0500

    Add files via upload

---
+++ 14guestarrayv2/14augarrayv2.js
@@ -0,0 +1,5 @@
+var Guest = ['Kareem', 'Raheem'];
+for (var i = 0; i < Guest.length; i++) {
+    //console.log('I welcome you Sir!' + Guest[i]);
+    console.log('Dear Mr. ' + Guest[i] + ',\n\n Please be informed that you have been invited by us today at sharp 9.00 pm!' + '\n\n');
+}
---
+++ 14guestarrayv2/14augarrayv2.ts
@@ -0,0 +1,6 @@
+let Guest :string[] = ['Kareem','Raheem'];
+for(let i=0; i<Guest.length;i++){
+    //console.log('I welcome you Sir!' + Guest[i]);
+    console.log('Dear Mr. ' + Guest[i] + ',\n\n Please be informed that you have been invited by us today at sharp 9.00 pm!'  + '\n\n');
+    
+}
