80edda0569c4acd10e8dd13c0740b377f86496ef
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 12:07:02 +0500

    Add files via upload

---
+++ 27color/27color.ts
@@ -0,0 +1,69 @@
+
+
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+
+
+
+
+// • If the alien is yellow, print a message that the player earned 10 points.
+
+// • If the alien is red, print a message that the player earned 15 points.
+
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+//1.for green color test 
+let acolor : string = "green";
+if(acolor === "green"){
+console.log("you eraned 5 Point score");
+}
+else if(acolor === "yellow"){
+    console.log("you eraned 10 Point score");
+}else if(acolor === "red"){
+    console.log("you eraned 15 Point score");
+}
+else{
+    console.log("plz select among three mentioned color");
+}
+
+//2.for yellow color test 
+ acolor  = "yellow";
+if(acolor === "green"){
+console.log("you eraned 5 Point score");
+}
+else if(acolor === "yellow"){
+    console.log("you eraned 10 Point score");
+}else if(acolor === "red"){
+    console.log("you eraned 15 Point score");
+}
+else{
+     console.log("plz select among three mentioned color");
+}
+
+//3.for red color test 
+ acolor  = "red";
+if(acolor === "green"){
+console.log("you eraned 5 Point score");
+}
+else if(acolor === "yellow"){
+    console.log("you eraned 10 Point score");
+}else if(acolor === "red"){
+    console.log("you eraned 15 Point score");
+}
+else{
+     console.log("plz select among three mentioned color");
+}
+
+//3.for red color test 
+ acolor  = "pink";
+if(acolor === "green"){
+console.log("you eraned 5 Point score");
+}
+else if(acolor === "yellow"){
+    console.log("you eraned 10 Point score");
+}else if(acolor === "red"){
+    console.log("you eraned 15 Point score");
+}
+else{
+     console.log("plz select among three mentioned color");
+}
