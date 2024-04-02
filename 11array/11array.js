d1e379ed7d060dc57bac79d74f20d5b65c10ac83
hajikhan110 <156642658+hajikhan110@users.noreply.github.com>
2024-04-02 11:49:03 +0500

    Add files via upload

---
+++ 11array/11array.js
@@ -0,0 +1,4 @@
+var nms = ['Karee', 'Raheem'];
+for (var i = 0; i < nms.length; i++) {
+    console.log(nms[i]);
+}
---
+++ 11array/11array.ts
@@ -0,0 +1,4 @@
+let nms :string[]=['Karee','Raheem'];
+for(let i=0; i<nms.length; i++){
+    console.log(nms[i]);
+}
